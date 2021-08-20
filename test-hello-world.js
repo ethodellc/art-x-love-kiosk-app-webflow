7026c9aab6297f8ecbc95aebf7f878d69461fea2
Jason Ables <jason@ethode.com>
2021-08-20 18:06:36 -0400

    Tests Github Pages as a CDN for webflow
    Use I spelled "the" wrong on purpose ;)

--- test-hello-world.js
+++ test-hello-world.js
@@ -3,4 +3,5 @@ alert('Hello, Webflow!');
 function testExternalFunctionCall() {
   alert('Hello from inside a function in an externally loaded JS file from within Webflow.');
   alert('Testhing cache process in Webflow');
+  alert('Github Pages for teh win?');
 }
