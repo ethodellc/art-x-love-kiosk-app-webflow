defd29eaf98d7b4fbac6765f9882e351432797e2
Jason Ables <jason@ethode.com>
2021-08-20 17:07:48 -0400

    Adds a caching test

--- test-hello-world.js
+++ test-hello-world.js
@@ -2,4 +2,5 @@ alert('Hello, Webflow!');
 
 function testExternalFunctionCall() {
   alert('Hello from inside a function in an externally loaded JS file from within Webflow.');
+  alert('Testhing cache process in Webflow');
 }
