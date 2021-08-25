b72bc7f09fea69d1f2e00953d49b7b57c47c6e9e
Jason Ables <jason@ethode.com>
2021-08-24 21:38:59 -0400

    Manually configures virtual keyboard as opposed to using JSON (CORS error)
    https://trello.com/c/cnn9qADR/47-8-23-21-create-a-dynamic-keyboard-to-use-on-the-find-my-brick-page

--- pages/find-my-brick.js
+++ pages/find-my-brick.js
@@ -8,14 +8,47 @@ document.addEventListener('DOMContentLoaded', function (event) {
      * Have to define an Array of Objects for the custom keys. Hint: Each object creates a row element (HTML) on the keyboard.
      * e.g. [{"key":"value"}, {"key":"value"}] => [{"0":"A","1":"B","2":"C"}, {"0":"D","1":"E","2":"F"}]
      */
-    keysArrayOfObjects: null,
+    keysArrayOfObjects: [
+      {
+        "0": "Q",
+        "1": "W",
+        "2": "E",
+        "3": "R",
+        "4": "T",
+        "5": "Y",
+        "6": "U",
+        "7": "I",
+        "8": "O",
+        "9": "P"
+      },
+      {
+        "0": "A",
+        "1": "S",
+        "2": "D",
+        "3": "F",
+        "4": "G",
+        "5": "H",
+        "6": "J",
+        "7": "K",
+        "8": "L"
+      },
+      {
+        "0": "Z",
+        "1": "X",
+        "2": "C",
+        "3": "V",
+        "4": "B",
+        "5": "N",
+        "6": "M"
+      }
+    ],
 
     /*!
      * Required only if "keysArrayOfObjects" is "null".
      * The path of the "kioskboard-keys-${langugage}.json" file must be set to the "keysJsonUrl" option. (XMLHttpRequest to getting the keys from JSON file.)
      * e.g. '/Content/Plugins/KioskBoard/dist/kioskboard-keys-english.json'
      */
-    keysJsonUrl: "//cdn.jsdelivr.net/npm/kioskboard@1.4.0/dist/kioskboard-keys-english.json",
+    keysJsonUrl: NULL,
 
     /*
      * Optional: (Special Characters Object)
